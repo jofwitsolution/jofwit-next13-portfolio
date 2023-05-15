861423502286a70ca1f446285085109ede857f32
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2023-05-15 18:02:12 +0100

    segment

--- app/api/abouts/create/route.js
+++ app/api/abouts/create/route.js
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 import cloudinary from "cloudinary";
-import formidable from 'formidable';
-import multer from "multer"
+import formidable from "formidable";
+import multer from "multer";
 import { createEdgeRouter, createRouter, expressWrapper } from "next-connect";
 import parser from "body-parser";
 
@@ -16,18 +16,18 @@ cloudinary.v2.config({
 
 const upload = multer({
   storage: multer.diskStorage({
-    destination: './public/uploads', // replace with your desired folder path
+    destination: "./public/uploads", // replace with your desired folder path
     filename: (req, file, callback) => {
-      console.log(file)
-      console.log("Fired")
-      callback(null, Date.now() + '-' + file.originalname);
+      console.log(file);
+      console.log("Fired");
+      callback(null, Date.now() + "-" + file.originalname);
     },
   }),
 });
 
 const uploader = cloudinary.uploader;
 
-const router = createRouter()
+const router = createRouter();
 
 // router.use(expressWrapper(parser.json()));
 // router.use(upload.single('file'));
@@ -35,25 +35,22 @@ router.post(async (req, event, next) => {
   // const body = await req.formData()
   // console.log(req.file)
   // console.log(body)
-  const date = Date.now()
-  console.log(date)
+  const date = Date.now();
+  console.log(date);
 
   const form = formidable();
   return form.parse(req, (err, fields, files) => {
-    console.log(files)
+    console.log(files);
 
-    return new Response(JSON.stringify({message: "Done"}), {status: 200})
-  })
-  
+    return new Response(JSON.stringify({ message: "Done" }), { status: 200 });
+  });
 });
 
-export const config = {
-  api: { bodyParser: false },
-};
+export const api = { bodyParser: false };
 
 export async function POST(req, context) {
   // NextResponse.json({message: "Done"})
-  return router.run(req, context)
+  return router.run(req, context);
 }
 
 // export const POST = async (req) => {
